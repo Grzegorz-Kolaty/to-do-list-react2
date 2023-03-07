5ac77cfd7339b42d98e72b4a183b7ff0a6f376b7
Grzegorz Kolaty <65914486+Grzegorz-Kolaty@users.noreply.github.com>
2023-03-07 15:48:07 +0100

    complete rework

---
+++ src/Form/index.js
@@ -0,0 +1,10 @@
+import "./style.css";
+
+const Form = () => (
+    <form className="form">
+        <input type="text" placeholder="Co jest do zrobienia?" className="form__input" autoFocus/>
+        <button className="form__button">Dodaj zadanie</button>
+    </form>
+);
+
+export default Form;
---
+++ src/Tasks/index.js
@@ -0,0 +1,25 @@
+import "./style.css";
+
+const Tasks = (props) => (
+  <ul className="list">
+    {props.tasks.map(task => (
+      <li key={task.id}
+        className={`list__item${task.done && props.hideDoneTasks ? " list__item--hidden" : ""}`}
+      >
+        <button className="task__checkButton task__checkButton--true">
+          {task.done ? "✔" : ""}
+        </button>
+
+        <span className={`task__item${task.done
+          ? " list__item--done"
+          : ""}`}>
+          {task.content}
+        </span>
+
+        <button className="task__removeButton">🗑</button>
+      </li>
+    ))}
+  </ul >
+);
+
+export default Tasks;
