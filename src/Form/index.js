3c853b326872e6c9bfd639d8d492d21c5b4e0123
Grzegorz Kolaty <65914486+Grzegorz-Kolaty@users.noreply.github.com>
2023-03-08 12:53:49 +0100

    readme updates

--- src/Form/index.js
+++ src/Form/index.js
@@ -2,7 +2,7 @@ import "./style.css";
 
 const Form = () => (
     <form className="form">
-        <input type="text" placeholder="Co jest do zrobienia?" className="form__input" autoFocus/>
+        <input type="text" placeholder="Co jest do zrobienia?" className="form__input" autoFocus />
         <button className="form__button">Dodaj zadanie</button>
     </form>
 );
