f65872fc71955d1eb024e64a1b05747458c1923e
Grzegorz Kolaty <grzegorzkolaty@gmail.com>
2023-04-12 17:49:29 +0200

    code formatting

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # Task List with YouCode!
 
 ## 💪 To do list - how to use❔
+
 This website has only one specific function, look:
 👆 https://grzegorz-kolaty.github.io/to-do-list-react2/ 👆
 
 <img src="public/images/how_to_use.gif" alt="howToUse" width="600"/>
 
 ## 🧑 Do not forget to see my homepage❕
+
 My homepage 👆 https://grzegorz-kolaty.github.io/homepage/ 👆
 
 Please do not forget to check my other projects on my homepage - trying to be best developer you can find here :)
@@ -16,6 +18,7 @@ Please do not forget to check my other projects on my homepage - trying to be be
 Currency converter 👆 https://grzegorz-kolaty.github.io/currency-converter/ 👆
 
 ## ⚙ Used technologies and languages
+
 - HTML,
 - CSS,
 - BEM,
--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,26 +1,26 @@
-import "./style.css";
+import './style.css'
 
-const Tasks = (props) => (
-  <ul className="list">
+const Tasks = props => (
+  <ul className='list'>
     {props.tasks.map(task => (
       <li
         key={task.id}
-        className={`list__item${task.done && props.hideDoneTasks ? " list__item--hidden" : ""}`}
+        className={`list__item${
+          task.done && props.hideDoneTasks ? ' list__item--hidden' : ''
+        }`}
       >
-        <button className="task__checkButton task__checkButton--true">
-          {task.done ? "✔" : ""}
+        <button className='task__checkButton task__checkButton--true'>
+          {task.done ? '✔' : ''}
         </button>
 
-        <span className={`task__item${task.done
-          ? " list__item--done"
-          : ""}`}>
+        <span className={`task__item${task.done ? ' list__item--done' : ''}`}>
           {task.content}
         </span>
 
-        <button className="task__removeButton">🗑</button>
+        <button className='task__removeButton'>🗑</button>
       </li>
     ))}
   </ul>
-);
+)
 
-export default Tasks;
+export default Tasks
