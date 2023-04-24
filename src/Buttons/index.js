178f69e2b9a0f98813d5bad49ace9accf4d32b7d
Grzegorz Kolaty <grzegorzkolaty@gmail.com>
2023-04-24 13:06:57 +0200

    counter next to buttons, visual updates

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,30 +1,27 @@
 import "./style.css";
 
 const Buttons = ({ tasks, hideDone, toggleHideDone, setAllDone }) => {
-  const taskDoneCount = tasks.filter(task => task.done === true).length;
+  const taskUnDoneCount = tasks.filter(task => task.done === false).length;
   const taskHideDoneCount = tasks.filter(task => task.done === true && hideDone === true).length;
 
   return (
     <div className="buttons">
       {tasks.length > 0 && (
         <>
-          <div className="buttons__hiding">
-            <button
-              onClick={toggleHideDone}
-              className="buttons__button">
-              {hideDone ? "Pokaż" : "Ukryj"} ukończone
-            </button>
-            {taskHideDoneCount > 0 ? `Zadań ukrytych: ${taskHideDoneCount}` : ""}
-          </div>
-          <div className="buttons__done">
-            <button
-              onClick={setAllDone}
-              className="buttons__button"
-              disabled={tasks.every((task) => task.done)}>
-              Ukończ wszystkie
-            </button>
-            {taskDoneCount === 0 ? "Brak ukończonych!" : `Ukończonych zadań: ${taskDoneCount}`}
-          </div>
+          <button
+            onClick={toggleHideDone}
+            className="buttons__button">
+            {hideDone ? "Pokaż" : "Ukryj"} ukończone
+            {taskHideDoneCount > 0 ? ` (${taskHideDoneCount})` : ""}
+          </button>
+
+          <button
+            onClick={setAllDone}
+            className="buttons__button"
+            disabled={tasks.every((task) => task.done)}>
+            Ukończ wszystkie
+            {taskUnDoneCount > 0 ? ` (${taskUnDoneCount})` : ""}
+          </button>
         </>
       )}
     </div>
