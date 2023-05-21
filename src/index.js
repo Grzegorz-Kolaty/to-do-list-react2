39a03bc5786e8069d4913153580178de1a3a1714
Grzegorz Kolaty <grzegorzkolaty@gmail.com>
2023-05-21 11:58:45 +0200

    application split into features

--- src/index.js
+++ src/index.js
@@ -5,6 +5,45 @@ import reportWebVitals from "./reportWebVitals";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./theme";
 import { GlobalStyle } from "./GlobalStyle";
+import { configureStore } from "@reduxjs/toolkit";
+
+const initialState = {
+  tasks: [],
+};
+
+const tasksReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "addTask":
+      return {
+        ...state,
+        tasks: [
+          ...state.tasks,
+          {
+            content: action.payload
+          },
+        ]
+      };
+    default:
+      return state;
+  }
+};
+
+const addTask = content => ({
+  type: "addTask",
+  payload: content,
+});
+
+const selectTasks = ({ tasks }) => tasks;
+
+const store = configureStore({ reducer: tasksReducer });
+console.log(selectTasks(store.getState()));
+
+
+store.dispatch(addTask("zadanie1"));
+console.log(selectTasks(store.getState()));
+
+store.dispatch(addTask("zadanie2"));
+console.log(selectTasks(store.getState()));
 
 
 const container = document.getElementById("root")
