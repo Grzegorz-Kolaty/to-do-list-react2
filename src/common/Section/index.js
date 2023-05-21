2b07b6753a28a3b10787643bd5e2eb4349ca696b
Grzegorz Kolaty <grzegorzkolaty@gmail.com>
2023-05-21 14:20:37 +0200

    bold font added to titles

--- src/common/Section/index.js
+++ src/common/Section/index.js
@@ -2,7 +2,7 @@ import { Sections, Title, Body } from "./styled";
 
 const Section = ({ title, body, extraHeaderContent }) => (
   <Sections>
-    <Title>{title}{extraHeaderContent}</Title>
+    <Title><b>{title}</b>{extraHeaderContent}</Title>
     <Body>
       {body}
     </Body>
