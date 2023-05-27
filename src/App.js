09cdc96951db7e7449ff47d09f430af7633dcc38
Grzegorz Kolaty <grzegorzkolaty@gmail.com>
2023-05-27 15:17:48 +0200

    navigation bar stylized as styled component

--- src/App.js
+++ src/App.js
@@ -2,35 +2,35 @@ import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom/cjs/rea
 import TasksPage from "./features/tasks/TasksPage/index";
 import AuthorPage from "./features/author/AuthorPage";
 import TaskPage from "./features/tasks/TaskPage/index";
-import { StyledNavLink } from "./styled";
+import { Nav, List, Item, StyledNavLink } from "./styled";
 
 function App() {
   return (
     <BrowserRouter basename="/to-do-list-react2">
-      <nav>
-        <ul>
-          <li>
+      <Nav>
+        <List>
+          <Item>
             <StyledNavLink to="/zadania">Zadania</StyledNavLink>
-          </li>
-          <li>
+          </Item>
+          <Item>
             <StyledNavLink to="/author">O autorze</StyledNavLink>
-          </li>
-        </ul>
-        <Switch>
-          <Route path="/zadania/:id">
-            <TaskPage />
-          </Route>
-          <Route path="/zadania">
-            <TasksPage />
-          </Route>
-          <Route path="/author">
-            <AuthorPage />
-          </Route>
-          <Route path="/">
-            <Redirect to="/zadania" />
-          </Route>
-        </Switch>
-      </nav>
+          </Item>
+        </List>
+      </Nav>
+      <Switch>
+        <Route path="/zadania/:id">
+          <TaskPage />
+        </Route>
+        <Route path="/zadania">
+          <TasksPage />
+        </Route>
+        <Route path="/author">
+          <AuthorPage />
+        </Route>
+        <Route path="/">
+          <Redirect to="/zadania" />
+        </Route>
+      </Switch>
     </BrowserRouter>
   );
 }
--- src/styled.js
+++ src/styled.js
@@ -1,8 +1,28 @@
 import { NavLink } from "react-router-dom/cjs/react-router-dom"
 import styled from "styled-components"
 
+export const Nav = styled.nav`
+    background-color: ${({ theme }) => theme.background.colorTeal};
+    color: ${({ theme }) => theme.font.colorWhite};
+`;
+
+export const List = styled.ul`
+    ${({ theme }) => theme.spacingElements};
+    display: flex;
+    flex-flow: row wrap-reverse;
+    justify-content: center;
+    list-style: none;
+`;
+
+export const Item = styled.li`
+    ${({ theme }) => theme.spacingBoxes};
+`;
+
 export const StyledNavLink = styled(NavLink)`
+    color: ${({ theme }) => theme.font.colorWhite};
+    text-decoration: none;
+
     &.active {
-        color: red;
+        font-weight: bold;
     }
 `;
