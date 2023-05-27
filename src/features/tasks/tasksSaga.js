44d0e9f4905af832b8ad212594bfe1e1f3996719
Grzegorz Kolaty <grzegorzkolaty@gmail.com>
2023-05-27 19:56:18 +0200

    delay set to 2 sec

--- src/features/tasks/tasksSaga.js
+++ src/features/tasks/tasksSaga.js
@@ -4,22 +4,21 @@ import { getExampleTasks } from "./getExampleTasks";
 import { saveTasksInLocalStorage } from "./tasksLocalStorage";
 
 function* fetchExampleTasksHandler() {
-    try {
-        yield delay(1000);
-        const exampleTasks = yield call(getExampleTasks);
-        yield put(setTasks(exampleTasks));
-    } catch (error) {
-        yield call(alert, error);
-    }
+  try {
+    yield delay(2000);
+    const exampleTasks = yield call(getExampleTasks);
+    yield put(setTasks(exampleTasks));
+  } catch (error) {
+    yield call(alert, error);
+  }
 }
 
 function* saveTasksInLocalStorageHandler() {
-    const tasks = yield select(selectTasks);
-    yield call(saveTasksInLocalStorage, tasks);
+  const tasks = yield select(selectTasks);
+  yield call(saveTasksInLocalStorage, tasks);
 }
 
 export function* tasksSaga() {
-    yield takeLatest(fetchExampleTasks.type, fetchExampleTasksHandler);
-    yield takeEvery("*", saveTasksInLocalStorageHandler);
+  yield takeLatest(fetchExampleTasks.type, fetchExampleTasksHandler);
+  yield takeEvery("*", saveTasksInLocalStorageHandler);
 }
-
