ca0ad9815097cf224e001a251db8f28114a289da
Grzegorz Kolaty <grzegorzkolaty@gmail.com>
2023-05-28 14:12:53 +0200

    author page filled with content

--- src/features/author/AuthorPage.js
+++ src/features/author/AuthorPage.js
@@ -7,7 +7,18 @@ function Author() {
     <Container>
       <Header title="O Autorze" />
       <Section title="Grzegorz Kolaty"
-        body={<>nanana</>} />
+        body={
+          <>
+            <p>
+              <b>Najbardziej zajmuje mnie nauka programowania i gry na gitarze!</b>🎸<br />
+              Obie rzeczy rozpocząłem stosunkowo niedawno i staram się im poświęcić każdą wolną chwilę.<br />
+              Interesuję się dietetyką🥑 co w konsekwencji zahacza o <i>medycynę🥼 i chemię🧪</i>.<br />
+              Obecnie jestem junior testerem szukującym się do pierwszego egzaminu z ISTQB, życzcie mi powodzenia!<br />
+            </p>
+            Mam nadzieję, że strona Ci się spodobała, warto zajrzeć co tutaj jest pod spodem - wprowadziłem całe mnóstwo rzeczy tj. <b>React-saga czy error handling</b>
+          </>
+        }
+      />
     </Container>
   )
 }
