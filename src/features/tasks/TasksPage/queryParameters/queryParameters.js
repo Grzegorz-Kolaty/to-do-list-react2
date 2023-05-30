850f2a92b7a79adf3e711520a0f13c3d9a6b5cba
Grzegorz Kolaty <grzegorzkolaty@gmail.com>
2023-05-30 22:44:47 +0200

    console.logs removed

--- src/features/tasks/TasksPage/queryParameters/queryParameters.js
+++ src/features/tasks/TasksPage/queryParameters/queryParameters.js
@@ -2,7 +2,6 @@ import { useLocation, useHistory } from "react-router-dom";
 
 export const useQueryParameter = (searchQueryParamName) => {
   const location = useLocation();
-  console.log(searchQueryParamName);
   const query = (new URLSearchParams(location.search)).get(searchQueryParamName);
   return query;
 };
@@ -13,7 +12,6 @@ export const useReplaceQueryParameter = () => {
 
   const replaceQueryParameter = ({ key, value }) => {
     const query = new URLSearchParams(location.search);
-    console.log(location.search);
     if (value !== undefined) {
       query.set(key, value);
     } else {
