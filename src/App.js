dfbe3d7f9d6776624eafa48a345a53f367d92137
Grzegorz Kolaty <grzegorzkolaty@gmail.com>
2023-06-01 20:54:18 +0200

    removed asterisk Redirect

--- src/App.js
+++ src/App.js
@@ -30,9 +30,6 @@ function App() {
         <Route path="/">
           <Redirect to="/zadania" />
         </Route>
-        <Route path="*">
-          <Redirect to="/zadania" />
-        </Route>
       </Switch>
     </HashRouter>
   );
